9ed6e61b821058f5411a674fadc55890050a8e0f
Rajat Nirwan <rajat@umd.edu>
2025-04-11 21:34:49 -0400

    update#

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; font-src 'self' https: data:; connect-src *;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
